843874a3d56f67cc859a4f7d12cf2f85b1843419
Maykon G Pedro <maykonglaffite@gmail.com>
2021-06-15 22:52:33 -0300

    add visualização dos dados

---
+++ data-raw/02-visualizando-dados.R
@@ -0,0 +1,87 @@
+
+
+# Instalar e carregar pacotes ---------------------------------------------
+if(!require("pacman")) install.packages("pacman")
+pacman::p_load(readr, tidyverse)
+
+
+# Carregar base -----------------------------------------------------------
+base <- readr::read_rds("./data/dados_capao.rds")
+
+
+# Explorar dados ----------------------------------------------------------
+
+# tipos de colunas
+base %>% dplyr::glimpse()
+
+# quantidade de linhas
+nrow(base)
+
+
+
+# criar categorias
+base_classes <-
+  base %>% 
+  dplyr::mutate(
+    classes_2007 = cut(dap_2007,
+                       seq(from = 0, to = 70, by = 5)),
+    classes_2010 = cut(dap_2010,
+                       seq(from = 0, to = 70, by = 5)),
+    classes_2013 = cut(dap_2013,
+                       seq(from = 0, to = 70, by = 5)),
+    classes_2016 = cut(dap_2016,
+                       seq(from = 0, to = 70, by = 5))
+    
+    )
+  
+# ver output
+view(base_classes)
+  
+
+# Visualizar dados --------------------------------------------------------
+
+# criar base para o gráfico
+base_classes %>%
+  # selecionar colunas das classes
+  dplyr::select(classes_2007,
+         classes_2010,
+         classes_2013,
+         classes_2016) %>%
+  # transformar em pivot
+  tidyr::pivot_longer(cols = 1:4,
+               names_to = "ano",
+               values_to = "classe") %>%
+  # sumarizar
+  dplyr::group_by(ano, classe) %>%
+  dplyr::summarise(n = n()) %>%
+  # retirar NA
+  dplyr::filter(!is.na(classe)) %>%
+  # modificar colunas
+  dplyr::mutate(
+    # deixar somente os anos
+    ano = stringr::str_remove_all(string = ano, pattern = "classes_"),
+    # retirar parentesês e colchetes
+    classe = stringr::str_remove_allstr_remove_all(string = classe, "\\(|\\]"),
+    # susbtitir vírgula por traço
+    classe = stringr::str_remove_allstr_replace_all(string = classe,",","-")
+    ) %>% 
+  
+# montando o gráfico
+  ggplot() +
+  geom_col(aes(x = classe, y = n)) +
+  scale_y_continuous(expand = c(0,0), limits = c(0, 6000)) +
+  facet_wrap(~ ano) +
+  #scale_fill_viridis_c(option = "magma") +
+  theme_bw() 
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
